4c34cdf54b5068d99910a3944591fb5e6e0f0ecc
rleenrwd <210642658+rleenrwd@users.noreply.github.com>
2026-09-24 00:37:27 -0700

    Configure EF Core SQL Server connection

--- backend/Fidora.Api/Program.cs
+++ backend/Fidora.Api/Program.cs
@@ -1,6 +1,14 @@
+using Fidora.Api.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+
+builder.Services.AddDbContext<FidoraDbContext>(options => 
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("FidoraDatabase")));
+    
 
 var app = builder.Build();
 
